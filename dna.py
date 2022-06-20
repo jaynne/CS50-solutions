8dc1f72f8e98d417929ab5b1cfc56f27f667cb53
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-06-20 20:58:11 -0300

    Dna - pset6

--- dna.py
+++ dna.py
@@ -2,10 +2,8 @@
 import csv
 import sys
 
-# Example below should return Lavender
-# python dna.py databases/large.csv sequences/5.txt
-# name,AGATC,TTTTTTCT,AATG,TCTAG,GATA,TATC,GAAA,TCTG
-# 22,33,43,12,26,18,47,41
+# Criamos listas com os STRs do csv escolhido e uma lista de 
+# pessoas (sendo cada uma um dicionário)
 
 STR = []
 people = []
@@ -20,20 +18,18 @@ with open(sys.argv[1], "r") as file:
         for data in person:
             # Item é o nome da chave do dicionário
             if data != 'name':
-                if data not in STR:
-                    STR.append(data)  # Assim evitamos uma lista repetida
+                if data not in STR:  # Assim evitamos uma lista repetida
+                    STR.append(data)
                 person[data] = int(person[data])
                 people.append(person)
 
-oc_list = []  # lista que contém o dicionário abaixo
 ocurrences = {}
 count = 0
 maxcount = 0
 j = 0
 
-# Dois valores p ocorrência = STR_oc e STR_max
-# STR_oc vai ser o "buffer", consideraremos apenas STR_max
-# na lista final
+# Count armazena temporariamente o valor das repetições.
+# consideraremos apenas maxcount no dicionário
 
 for n in range(len(STR)):
     ocurrences[STR[n]] = 0
@@ -41,7 +37,6 @@ for n in range(len(STR)):
 # Vamos criar uma lista com os maxcounts
 dna = (open(sys.argv[2], "r")).read()
 maxcount = 0
-STRcount = []
 for n in range(len(STR)):
     # Zeramos maxcount p cada STR
     maxcount = 0
@@ -52,23 +47,21 @@ for n in range(len(STR)):
         if dna[i:(i + len(cSTR))] == cSTR:
             cursor = 0
             while dna[i + cursor:(i + cursor + len(cSTR))] == cSTR:
-                count = count + 1
+                count += 1
                 cursor = cursor + len(cSTR)
                 if count > maxcount:
                     maxcount = count
                     ocurrences[cSTR] = maxcount
-    STRcount.append(ocurrences[cSTR])
 
 # Se temos todos os itens iguals, i.e len(STR), temos a correspondência
-
-name = 'No match'
+name = 'No match' # Caso nenhum nome seja encontrado, esse é o default
 for person in people:
     count = 0
     # Checa a correspondência com cada pessoa
     for data in person:  # Pula a parte dos nomes
         if data != 'name':
             if person[data] == ocurrences[data]:
-                count = count + 1
+                count += 1
             if count == len(STR):
                 name = person['name']
 print(name)
