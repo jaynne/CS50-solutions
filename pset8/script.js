040fe76c955999bf1b3ca57241c7316e2dd9ac10
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-06-20 20:58:12 -0300

    Homepage - pset8

--- pset8/script.js
+++
@@ -1,7 +0,0 @@
-$('img').load(function() {
-    $(this).data('height', this.height);
-}).bind('mouseenter mouseleave', function(e) {
-    $(this).stop().animate({
-        height: $(this).data('height') * (e.type === 'mouseenter' ? 1.5 : 1)
-    });
-});
